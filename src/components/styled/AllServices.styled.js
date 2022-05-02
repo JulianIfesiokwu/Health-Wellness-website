d48166887b20c265ee2496e9825f250bbf4d84b8
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-05-02 20:45:23 +0100

    working on filter for services

--- src/components/styled/AllServices.styled.js
+++ src/components/styled/AllServices.styled.js
@@ -2,104 +2,124 @@ import styled from "styled-components";
 
 const AllServices = styled.div`
   padding: 4rem;
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
 
-  .service-card {
-    width: 32%;
-    border: 1px solid #d3d3d3;
-    border-radius: 5px;
+  .filter-controls {
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 1.5rem;
+    justify-content: center;
+    margin-bottom: 2rem;
 
-    &:hover {
-      box-shadow: 0px 2px 4px rgba(40, 41, 61, 0.04),
-        0px 8px 16px rgba(96, 97, 112, 0.16);
-      cursor: pointer;
+    h3 {
+      font-family: "Open Sans", "sans-serif";
+      font-weight: 300;
+      color: #155bd5;
+      text-align: center;
     }
 
-    .img-container {
-      width: 100%;
-      img {
-        width: 100%;
-        height: auto;
-        object-fit: contain;
-        border-radius: 5px 5px 0 0;
-        display: block;
-      }
-    }
-
-    .details {
-      padding: 1rem;
-      h4 {
-        font-size: 1.5rem;
-        margin-bottom: 0.8rem;
-        color: #155bd5;
-        font-family: "Open Sans", "sans-serif";
-        font-weight: 300;
-      }
-
-      p {
-        margin-bottom: 0.3rem;
-        line-height: 25px;
-
-        button {
-          color: blue;
-          background-color: transparent;
-          border: none;
-          cursor: pointer;
-        }
-      }
-
-      .description {
-        font-size: 0.9rem;
-      }
-
-      .price {
-        font-family: "Open Sans", "sans-serif";
-        font-weight: bold;
-        font-size: 1.3rem;
-      }
-
-      .schedule-btn {
-        font-size: 1rem;
-        width: 100%;
-        background-color: #155bd5;
-        color: white;
-
-        &:hover {
-          color: #155bd5;
-          background-color: white;
-        }
-      }
-      .massage-type {
-        font-weight: bold;
-      }
+    .icon {
+      vertical-align: middle;
     }
   }
 
-  @media (max-width: 768px) {
-    padding: 2rem;
+  .listed-services {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 
     .service-card {
+      width: 32%;
+      border: 1px solid #d3d3d3;
+      border-radius: 5px;
+      display: flex;
       flex-direction: column;
-      padding: 0;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 1.5rem;
+
+      &:hover {
+        box-shadow: 0px 2px 4px rgba(40, 41, 61, 0.04),
+          0px 8px 16px rgba(96, 97, 112, 0.16);
+        cursor: pointer;
+      }
 
       .img-container {
         width: 100%;
-
         img {
+          width: 100%;
+          height: auto;
+          object-fit: contain;
+          border-radius: 5px 5px 0 0;
           display: block;
         }
       }
 
       .details {
-        width: 100%;
+        padding: 1rem;
+        h4 {
+          font-size: 1.5rem;
+          margin-bottom: 0.8rem;
+          color: #155bd5;
+          font-family: "Open Sans", "sans-serif";
+          font-weight: 300;
+        }
+
+        p {
+          margin-bottom: 0.3rem;
+          line-height: 25px;
+
+          button {
+            color: blue;
+            background-color: transparent;
+            border: none;
+            cursor: pointer;
+          }
+        }
+
+        .description {
+          font-size: 0.9rem;
+        }
+
+        .price {
+          font-family: "Open Sans", "sans-serif";
+          font-weight: bold;
+          font-size: 1.3rem;
+        }
+
+        .schedule-btn {
+          font-size: 1rem;
+          width: 100%;
+          background-color: #155bd5;
+          color: white;
+
+          &:hover {
+            color: #155bd5;
+            background-color: white;
+          }
+        }
+        .massage-type {
+          font-weight: bold;
+        }
+      }
+    }
+
+    @media (max-width: 768px) {
+      padding: 2rem;
+
+      .service-card {
+        flex-direction: column;
+        padding: 0;
+
+        .img-container {
+          width: 100%;
+
+          img {
+            display: block;
+          }
+        }
+
+        .details {
+          width: 100%;
+        }
       }
     }
   }
--- src/pages/Services.page.jsx
+++ src/pages/Services.page.jsx
@@ -4,24 +4,46 @@ import Button from "../components/styled/Button.styled"
 import AllServices from "../components/styled/AllServices.styled"
 import { massageTypes } from '../data'
 import OneService from "../components/OneService/OneService.component"
+import { AiOutlineSearch } from "react-icons/ai";
 
 const ServicesPage = () => {
+    
 
     return (
         <>
         <ServicesPageStyled>
-        <ServicesVideo />
-        <h1>Explore a wide range of affordable services.</h1>
-        <p>Schedule your session with any of our massage therapists either on site or in your home.</p>
-        <Button>Lets GO!</Button>
+            <ServicesVideo />
+            <h1>Explore a wide range of affordable services.</h1>
+            <p>Schedule your session with any of our massage therapists either on site or in your home.</p>
+            <Button>Lets GO!</Button>
         </ServicesPageStyled>
         <AllServices>
-            {massageTypes.map((massage) => {
-                const {id} = massage
-                return (
-                    <OneService key={id} massage={massage} />
-                )
-            })}
+            <div className="filter-controls">
+                <div className="price-control">
+                    <h3>Search services by filters <AiOutlineSearch className="icon" size='30'/></h3>
+                    <input type='range' />
+                    <form>
+                        <label htmlor='category'>Category:</label>
+                        <select name="category" id="category">
+                            {allCategories.map((category) => {
+                                return (
+                                <option value='category'>{category}</option>
+                                )
+                            })}
+                        </select>
+                        </form>
+                </div>
+
+            </div>
+            <div className="listed-services">
+                                {massageTypes.map((massage) => {
+                    const {id} = massage
+                    return (
+                        <OneService key={id} massage={massage} />
+                    )
+                })}
+
+            </div>
         </AllServices>
         </>
     )
